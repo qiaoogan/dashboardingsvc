084b66990944756d46f934f66c306534d2ad663f
qiaoogan <qiaogang1984@163.com>
2024-02-28 15:14:41 +0800

    update health

--- src/routes/app.route.js
+++ src/routes/app.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/health", function(req, res, next) {
-    res.send("Service is Alive");
+    res.send({ status: "alive", vesrion: process.env.DASHBACKEND_VERSION });
 });
 
 module.exports = router;

57fb6016f13cf01d9fe927e8090527565ee4d77b
qiaoogan <qiaogang1984@163.com>
2024-03-11 10:26:53 +0800

    debugging

--- apiTest/test-in-container-cloud.sh
+++ apiTest/test-in-container-cloud.sh
@@ -5,6 +5,7 @@ docker run \
   --mount type=bind,source="$(pwd)",target=/home/mount \
   -e TEST_ENV="container" \
   -e STOCKSERVICE_HOST="192.168.5.143" \
-  -e STOCKSERVICE_PORT="31102" \
+  -e STOCKSERVICE_PORT="31101" \
+  -e BACKENDSEVER_PORT="31102" \
   qiaoogan/nodejestenv:0.0.1 \
   bash /home/mount/apiTest/tests-entry.sh

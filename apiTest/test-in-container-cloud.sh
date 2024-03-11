889b7339c3bb7da6a778794f0f683a81c78199e2
qiaoogan <qiaogang1984@163.com>
2024-03-11 09:26:26 +0800

    change img

--- apiTest/test-in-container-cloud.sh
+++ apiTest/test-in-container-cloud.sh
@@ -6,5 +6,5 @@ docker run \
   -e TEST_ENV="container" \
   -e STOCKSERVICE_HOST="192.168.5.143" \
   -e STOCKSERVICE_PORT="31102" \
-  nodejestenv:0.0.1 \
+  qiaoogan/nodejestenv:0.0.1 \
   bash /home/mount/apiTest/tests-entry.sh
